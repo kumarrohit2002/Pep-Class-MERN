d1b9617be99d31f2efc9f3269a325fb9817cbf02
rohit Kumar <rohitranjanrrsingh@gmail.com>
2025-06-17 10:15:21 +0530

    Feat: Added Array Object

---
+++ day2/Array-and-object/app.js
@@ -0,0 +1,12 @@
+// object store key value pair keys should be string and symbol
+
+const obj={
+    name:'Rohit Kumar',
+    address:'Bihar',
+    course:{
+        branch:'CSE',
+        specilation:'Data Science'
+    }
+}
+
+console.log(obj);
