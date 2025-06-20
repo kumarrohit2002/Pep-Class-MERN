e0208ccdaad40add7681fcbca31e29d0ff98f51f
rohit Kumar <rohitranjanrrsingh@gmail.com>
2025-06-20 09:10:35 +0530

    Event and forms

---
+++ day4/form/script.js
@@ -0,0 +1,11 @@
+const onSubmitHandler = (e) => {
+
+}
+
+const title = document.getElementById('greetings');
+
+const handleUserName = (e) => {
+    console.log('typed')
+    console.log(e.target.value);
+    console.log('--------------------------------------')
+}
