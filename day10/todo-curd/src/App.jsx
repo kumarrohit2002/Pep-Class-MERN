de7172266cfd217c90aebaa2ceae683463003d0d
rohit Kumar <rohitranjanrrsingh@gmail.com>
2025-06-28 10:42:42 +0530

    create  to-do CURD Opration using useState

---
+++ day10/todo-curd/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Curd from './components/Curd'
+const App = () => {
+  return (
+    <div>
+      <h1>Hello from React</h1>
+        <Curd/>
+    </div>
+  )
+}
+
+export default App;
+
+
+// <button>Update</button>
+//                                     <button type="reset">Cancel</button>
